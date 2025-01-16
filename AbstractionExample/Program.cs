b7333b9ffd5e1feec982dfc016ff6482dffa5adf
abdndmn <abdndmn@hotmail.com>
2025-01-16 15:20:43 +0300

    Add project files.

---
+++ AbstractionExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using AbstractionExample;
+
+Employee Developer = new Developer
+{
+    Name = "Curtis",
+    Surname = "Jackson",
+    Department = "Development"
+
+};
+Developer.EmployeeInfo();
+Developer.Title();
+
+Employee ProjectManager = new ProjectManager
+{
+    Name = "Marshall",
+    Surname = "Matters",
+    Department = "Project Management"
+};
+ProjectManager.EmployeeInfo();
+ProjectManager.Title();
+
+Employee SalesRep = new SalesRep
+{
+    Name = "Andre",
+    Surname = "Young",
+    Department = "Sales Department"
+
+};
+SalesRep.EmployeeInfo();
+SalesRep.Title();
+
---
+++ AbstractionExample/Title.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AbstractionExample;
+
+namespace AbstractionExample
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+
+        public abstract void Title();
+
+        public void EmployeeInfo()
+        {
+            Console.WriteLine($"Ad: {Name}\nSoyad: {Surname}\nDepartman: {Department}");
+        }
+
+    }
+    public class Developer : Employee
+    {
+        public override void Title()
+        {
+            Console.WriteLine("Yazılımcı olarak çalışıyorum.\n");
+        }
+
+    }
+    public class ProjectManager : Employee
+    {
+        public override void Title()
+        {
+            Console.WriteLine("Proje yöneticisi olarak çalışıyorum.\n");
+        }
+    }
+
+    
+    public class SalesRep : Employee
+    {
+        public override void Title()
+        {
+            Console.WriteLine("Satış temsilcisi olarak çalışıyorum.\n");
+        }
+    }
+}
+
+
+
+
